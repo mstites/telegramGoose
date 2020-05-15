ce7b48a7ff553548b217333bac37c46811cba471
mstites <gjstites@gmail.com>
2020-05-15 09:22:59 -0400

    Sync resources

--- test.py
+++ test.py
@@ -9,8 +9,15 @@ t = datetime.datetime.now() + datetime.timedelta(minutes=1)
 # test = Event(time, 774796474, )
 
 row = {'time':t, 'user':774796474, 'action':'msg', 'content':'test'}
+row2 = {'time':t+datetime.timedelta(minutes=2), 'user':774796474, 'action':'msg', 'content':'test'}
 df = df.append(row, ignore_index=True)
+df = df.append(row2, ignore_index=True)
+df = df.reset_index(drop = True)
 print(df)
 
+nextEvent = df.iloc[0]
+print(nextEvent)
+df = df.drop(0)
+df = df.reset_index(drop = True)
 # df = df.reset_index()
-df.to_pickle('assets/events.pkl')
+# df.to_pickle('assets/events.pkl')
