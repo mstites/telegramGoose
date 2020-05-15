ca70b78af04f4649f183cb784e0bd17561db2b57
mstites <gjstites@gmail.com>
2020-05-15 00:29:13 -0400

    Write event handler

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
-import time
-time = time.localtime()
-print(time.tm_hour)
+import pandas as pd
+import datetime
+
+utc = datetime.datetime.now()
+columns = ['time', 'user', 'action', 'content']
+df = pd.DataFrame(columns = columns)
+
+#
+# row1 = {'time':utc, 'user':123, 'action':'msg', 'content':'hello world'}
+# row2 = {'time':utc - datetime.timedelta(days=10), 'user':1, 'action':'msg', 'content':'hello world'}
+#
+# df = df.append(row1, ignore_index=True)
+# df = df.append(row2, ignore_index=True)
+# df = df.sort_values(by='time')
+# df = df.reset_index()
+print(df)
+#
+df.to_pickle('assets/events.pkl')
+#
+# nextEvent = df.iloc[0]['time']
+# print(nextEvent)
+#
+# event = df.iloc[0]
+# test = event['action']
+# print(test)
+# print(type(test))
+# # sort by timestamp
+#
+# df = df.drop(0)
+# print(df)
